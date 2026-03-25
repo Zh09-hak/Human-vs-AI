3d466e924a83ac31b5af686fa47d2312f6e68d30
Zhores Hakobyan <136333664+Zh09-hak@users.noreply.github.com>
2026-03-25 23:09:49 +0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,17 +1,35 @@
 import pandas as pd
+import pickle
+import json
+
 from preprocess import build_tokenizer, texts_to_padded
 from model import build_model
 
-df = pd.read_csv('data/train.csv')
 
-X = df['text']
-y = df['generated']
+def main():
+    df = pd.read_csv('data/train.csv')
 
-token = build_tokenizer(X)
-X = texts_to_padded(token, X)
+    X = df['text']
+    y = df['generated']
 
-model = build_model(len(token.word_index)+1, 300)
+    token = build_tokenizer(X)
+    X = texts_to_padded(token, X)
 
-model.fit(X, y, validation_split=0.1, epochs=10)
+    vocab_size = len(token.word_index) + 1
+    max_len = 300
 
-model.save("model/model.keras")
+    model = build_model(vocab_size, max_len)
+
+    model.fit(X, y, validation_split=0.1, epochs=5, batch_size=128)
+
+    model.save('model.keras')
+
+    with open('tokenizer.pkl', 'wb') as f:
+        pickle.dump(token, f)
+
+    with open('config.json', 'w') as f:
+        json.dump({"max_len": max_len}, f)
+
+
+if __name__ == "__main__":
+    main()
